90f3995e2288c7cb258cfce874527d9876a433e0
God <theholehave178@163.com>
2013-04-12 23:15:33 +0800

    Initial Commit

---
+++ eHealth/EAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  EAppDelegate.h
+//  eHealth
+//
+//  Created by god on 13-4-12.
+//  Copyright (c) 2013年 god. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class EViewController;
+
+@interface EAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) EViewController *viewController;
+
+@end
---
+++ eHealth/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  eHealth
+//
+//  Created by god on 13-4-12.
+//  Copyright (c) 2013年 god. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EAppDelegate class]));
+    }
+}
